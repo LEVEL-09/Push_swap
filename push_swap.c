e89f58c0eb705a3313638ba9e8598eedb524f673
mkhoubaz <hichamohicham651@gmail.com>
2025-12-30 09:37:32 +0100

    Complut check double

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mkhoubaz <mkhoubaz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/25 10:32:43 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2025/12/29 18:00:20 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2025/12/30 09:36:22 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,21 @@ int	push_swap(int ac, char **stack)
 	int	i;
 	int	*p;
 	int	old_ac;
-	int	result;
+	int	result; // result or p
 
 	i = 1;
-	old_ac = ac;
+	old_ac = ac - 1;
 	p = malloc(sizeof(int) * (ac - 1));
 	if (!p)
 		exit(1);
 	while (ac-- > 1)
 	{
-		// printf("{%d}", ac-1);
 		if ((!validate_stack(*(stack+i))))
-		{
-			ft_printf("Error\n");
-			return (1);
-		}
+			return (free(p), ft_printf("Error\n"), 1);
 		result = ft_atoi((const char*)*(stack+i++));
-		p[ac - 1] = result;
+		p[i - 2] = result;
 	}
 	if (check_double(p, old_ac))
-	{
-		ft_printf("Error\n");
-		return (1);
-	}
+		return (free(p), ft_printf("Error\n"), 1);
 	return (0);
 }
--- push_swap_util.c
+++ push_swap_util.c
@@ -6,7 +6,7 @@
 /*   By: mkhoubaz <mkhoubaz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 17:59:40 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2025/12/29 18:07:02 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2025/12/30 09:35:23 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,20 @@ int	validate_stack(char *av)
 }
 int	check_double(int *p, int size)
 {
-	int	*i;
-	int	idx;
+	int	i;
+	int	j;
 
-	i = malloc(sizeof(int) * size);
-	if (!i)
-		return (-1);
-	ft_bzero(i, (size_t)size);
-	while (size-- > 0)
+	i = 0;	
+	while (i < size)
 	{
-		idx = p[size];
-		printf("%d\n", p[size]);
-		if (i[idx])
-			return (1);
-		i[idx] = 1;
+		j = i + 1;
+		while (j < size)
+		{
+			if (p[i] == p[j])
+				return (1);
+			j++;
+		}
+		i++;
 	}
 	return (0);
 }
