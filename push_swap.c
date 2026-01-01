f0dc22afd5a6c2b87a5f45c25fa4a0b671cc1ad4
mkhoubaz <hichamohicham651@gmail.com>
2026-01-01 02:28:36 +0100

    Start add stack

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mkhoubaz <mkhoubaz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/25 10:32:43 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2025/12/31 23:30:19 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2026/01/01 02:22:53 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@ int	push_swap(int ac, char **av)
 {
 	// All this can be in other file
 	char	**args;
-	int		i;
+	t_list	*head;
+	t_list	*new;
 	int		temp;
+	int		i;
 
 	i = 1;
+	head = NULL;
 	if (ac == 1)
-		return (0); // 0 or 1
+		return (0);
 	while (i < ac)
 	{
 		args = ft_split(*(av + i++), ' ');
@@ -29,6 +32,13 @@ int	push_swap(int ac, char **av)
 		while (*args)
 		{
 			temp = ft_atoi((const char *)*args);
+			if (!head)
+				head = ft_lstnew(temp);
+			else
+			{
+				new = ft_lstnew(temp);
+				ft_lstadd_back(&head, new);
+			}
 			args += 1;
 		}
 	}
